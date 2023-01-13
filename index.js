cdbb0557b7315a87c17e35bc460da68819de572e
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-13 12:07:39 +0000

    I've added a function which starts the timer when the user clicks the start quiz button and also the function hides the start screen when the button is pressed

--- index.js
+++ index.js
@@ -3,13 +3,53 @@
 //Everytime you get a question wrong you subtract 1 secfrom the time, if you get it right, there are no penalties (time remains the same), track and add to the score each question.
 
 //For questions we can create each question as an array objects//.
- 
+
+
+
+//These are my questions
 let questions = [
 
-{questions: "Which Pokemon did Ash pick at the start of his journey?", answers: ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"], correctAnswer: "Pikachu"}
+{questions: "Which Pokemon did Ash start of his journey with?", answers: ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"], correctAnswer: "Pikachu"},
+{questions: "How many Harry Potter Movies are there?", answers: ["3", "5", "6", "8"], correctAnswer: "8"},
+
 ];
 
+//This is a counter to track the current question
 let currentQuestion = 0;
+
+
+//This is a timer function to start when the user clicks 'Start Quiz' and hides the start-screen// 
+let startScreen = document.querySelector('#start-screen');
+let timer = document.querySelector('#time');
+let timeLeft = 30;
+
+function startQuizTimer() {
+startScreen.style.display = 'none';
+let timerInterval = setInterval(function(){
+    timeLeft--;
+    timer.textContent = timeLeft;
+    if(timeLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+    }, 1000);
+}
+
+//functions that execute when the user clicks start quiz button//
+
+
+function beginQuiz() {
+    startQuizTimer();
+
+}
+
+//Start Quiz Button//
+let startQuizBtn = document.querySelector('#start');
+
+startQuizBtn.addEventListener('click', beginQuiz);
+
 
 //index for the questions, data-index per question? store in variable?//
 
@@ -19,3 +59,27 @@ let currentQuestion = 0;
 //Use Event delegation, you only need 1 event listener, otherwise you will need 4 separate event listeners//.
 
 //localStorage to save the high score and display this on the same page and on the highscores.html//.
+
+
+//Add event listener to the start quiz button to start timer when the user clicks 'Start quiz'
+//Once they press start quiz, then we should show the first question//
+
+
+
+
+
+
+
+
+
+//Acceptance Criteria
+//Create a code quiz that contains the following requirements:
+
+// start button that when clicked a timer starts and the first question appears.
+
+//Questions contain buttons for each answer.
+//When answer is clicked, the next question appears
+//If the answer clicked was incorrect then subtract time from the clock
+//The quiz should end when all questions are answered or the timer reaches 0.
+
+//When the game ends, it should display their score and give the user the ability to save their initials and their score//
