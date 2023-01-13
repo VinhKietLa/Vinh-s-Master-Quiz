f4d22a6da77e94638c47e6050054f08c93fa0172
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-13 17:43:35 +0000

    I've added a function to display and clear the scores and a message to show when there are no published scores

--- highscore.js
+++ highscore.js
@@ -1,13 +1,31 @@
 let highScores = document.querySelector('#highscores');
-
-
+let clearScores = document.querySelector('#clear');
+let noScore = document.querySelector('#noScores');
 
 //This function will display the user initials and scores//
 function displayHighScores() {
 
+    let storedUser = localStorage.getItem("User");
+    let user = JSON.parse(storedUser);
+
+    let message = (user.name + ' ' + '- ' + user.score);
+
+    let li = document.createElement('li');
+
+    li.textContent = message;
     
-
-
+    highScores.appendChild(li);
+    isScores = true;
 }
 
-displayHighScores();
+if(localStorage.getItem("User") != undefined){
+    displayHighScores();
+} else {
+    noScore.textContent = 'No scores to show =('
+}
+
+
+clearScores.addEventListener('click', function (){
+localStorage.clear();
+location.reload();
+})
