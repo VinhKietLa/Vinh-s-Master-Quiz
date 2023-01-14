573772ac799d9fda2e529f53d5e06be6a142a9b9
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-14 13:23:18 +0000

    cleaning up the code and comments

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-//pseudo code//
-
-//Everytime you get a question wrong you subtract 1 secfrom the time, if you get it right, there are no penalties (time remains the same), track and add to the score each question.
-
-//For questions we can create each question as an array objects//.
-
-
-
-//These are my questions
+//These are my questions which I've stored into an array of objects.
 let userQuestions = [
 
 {questions: "Which Pokemon did Ash start of his journey with?", answers: ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"], correctAnswer: "Pikachu"},
@@ -16,34 +8,31 @@ let userQuestions = [
 {questions: "Which planet is closest to the sun?", answers: ["Neptune", "Jupiter", "Earth", "Mercury"], correctAnswer: "Mercury"}
 ];
 
-//This is a counter to track the current question
-let currentQuestion = 0;
-let currentScore = 0;
-let outOfTime = false;
+let currentQuestion = 0;//This is a counter to track the current question//
+let currentScore = 0; //This tracks the users current score and will increase if the user gets the question right.//
+let outOfTime = false; // This boolean will turn on and will display the user will the appropriate message if they run out of time//
 
-//This function will display the questions when the user clicks the start screen, in an unordered list//
+//This function will display the questions when the user clicks the start screen and will display them as button elements//
 let questionSection = document.querySelector('#questionSection');
 let questionTitle = document.querySelector('#question-title');
 let choices = document.querySelector('#choices'); //This accesses the div Choices//
 
 function displayQuestions() {
     
-    let question = userQuestions; //This is the current question//
-    questionSection.style.display = 'block'; //This makes the question section visible//
-    questionTitle.textContent = question[currentQuestion].questions; //This sets the H2 textContent to the value of the selected question//
+    let question = userQuestions; //This stores the array of objects in another variable for clarity //
+    questionSection.style.display = 'block'; //This makes the questionSection visible when the function is called//
+    questionTitle.textContent = question[currentQuestion].questions; //This sets the H2 textContent to the value of the current question using the current question as the index//
 
-    // let choices = document.querySelector('#choices'); //This accesses the div Choices//
-    let answers = userQuestions[currentQuestion].answers; //This stores the answers of the current in a variable called answers
+    let answers = userQuestions[currentQuestion].answers; //This stores the answers of the current in a variable called answers, once again using the currentQuestion as the index//
     console.log(answers);
     for (let i = 0; i < answers.length; i++) { //This loops through the answers and creates a button for each answer.
        
         let answer = answers[i];
         let button = document.createElement('button');
         button.textContent = answer;
-        button.id = "UserChoice";
-        console.log(answer);
         choices.appendChild(button);
-//This function will listen to all the answers displayed and will add 10points to the correct score or deduct 10secs from the timer and then call the question function again for the next question once an answer is selected.
+
+    //This function will listen to all the answers displayed and will add 10points to the correct score or deduct 5 secs from the timer and then call the question function again for the next question once an answer is selected.
 
     button.addEventListener("click", function(e) {
         e.stopPropagation();
@@ -62,7 +51,7 @@ function displayQuestions() {
             } 
             
             if (currentQuestion === userQuestions.length) {
-                endQuiz();
+                endQuiz(); //If the user has answered all questions then the endQuiz will execute.
             } else {
                 displayQuestions();
             }        
@@ -70,11 +59,7 @@ function displayQuestions() {
 };
 };
 
-
-
-
 //This function will run when the game has ended and will allow the user to submit their initials and after hitting submit they will be taken to the highscore page where they can clear highscore or go back to the quiz home page.
-
 
 let quizEnded = document.querySelector('#end-screen');
 let h2 = document.querySelector('#end-screen h2');
@@ -98,36 +83,33 @@ function endQuiz() { //This function hides the questions section and displays th
         finalScore.textContent = currentScore;
 
     }
-}
+};
+
 //This event listener takes the input from the user and sends their initials and scores to the highscore page when they press the submit btn.
 let initials = document.querySelector('#initials');
 
 submitBtn.addEventListener("click", function (event){
-    event.preventDefault();
-    let initial = initials.value;
-    let newUser  = {name: initial, score: currentScore};
+    event.preventDefault(); //This prevents the default behaviour when the user hits submit//
+    let initial = initials.value; //This is the value of the current input from the user in the ID initals//
+    let newUser  = {name: initial, score: currentScore}; //This creates a new object with the properties name + score with the values initial input and the users current score.
 
-    let users = localStorage.getItem("Users");
+    let users = localStorage.getItem("Users"); //This accesses localStorage and checks if an existing item called 'User' exists, if it exists it will return the value as a string.
 
-    console.log(users);
-    if (users) {
-        users = JSON.parse(users);
-        console.log(users);
-        users.push(newUser);
-        console.log(users);
+    if (users) { //This checks if users is true and not equal to null or undefined.
+        users = JSON.parse(users); //JSON.parse converts the string value into a Javascript object and is necessary because when items are added to localStorage they're stored as a string//
+        users.push(newUser);//This adds the new user objects to the array of existing users. 
 
     } else {
-        users = [newUser];
+        users = [newUser]; //If there are no existing items callled user, then we create a new array as the user item.
         console.log(users);
 
     }
-    localStorage.setItem("Users", JSON.stringify(users));
-    window.location.href = "highscores.html";
+    localStorage.setItem("Users", JSON.stringify(users));//The array is then turned into a string so that it can be added to localstorage using JSON.stringify.
+    window.location.href = "highscores.html";//The user is then redirected to the highscore pae.
 });
 
 
-
-//This is a timer function to start when the user clicks 'Start Quiz' and hides the start-screen// 
+//This is a timer function to start when the user clicks 'Start Quiz' and hides the start-screen, if the time hits 0 then the quiz will end// 
 let startScreen = document.querySelector('#start-screen');
 let timer = document.querySelector('#time');
 let timeLeft = 30;
@@ -141,9 +123,8 @@ let timerInterval = setInterval(function(){
         // Stops execution of action at set interval
         clearInterval(timerInterval);
         timer.textContent = 0;
-        // Calls function to create and append image
         outOfTime = true;
-       endQuiz();
+        endQuiz();//this function is called when the timer hits 0.//
       }
     }, 1000);
 }
@@ -159,6 +140,20 @@ let startQuizBtn = document.querySelector('#start');
 
 startQuizBtn.addEventListener('click', beginQuiz);
 
+
+
+
+
+
+
+
+
+
+//pseudo code//
+
+//Everytime you get a question wrong you subtract 1 secfrom the time, if you get it right, there are no penalties (time remains the same), track and add to the score each question.
+
+//For questions we can create each question as an array objects//.
 
 
 //Todo //
